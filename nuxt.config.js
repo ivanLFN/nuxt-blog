fe0cd02ce5420fe08e22be78773548a889d269cf
ivanLFN <LFN.current@yandex.com>
2024-02-03 18:02:39 +0700

    update(MenuNav.vue): adding bootstrap style fot nav menu

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,11 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css' }
+    ],
+    script: [
+      { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js', crossorigin: 'anonymous' }
     ]
   },
 
